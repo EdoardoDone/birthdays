e360b505ea76d64d5c6217bd46a056d82503adce
francescomenazza96 <francesco.menazza@student.h-farm.com>
2019-11-20 10:51:42 -0500

    check

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 #! /usr/bin/env python3
 
+import sys
 from mypackage.birthdays import return_birthday
 
-return_birthday('Albert Einstein')
-return_birthday('Alan Turing')
+if len(sys.argv) == 3:			
+    first_name = str(sys.argv[1])
+    last_name = str(sys.argv[2])
+    user_input = first_name + ' ' + last_name
+else:
+    print("Enter a valid name")
+    exit()
+
+
+return_birthday(user_input)
+
